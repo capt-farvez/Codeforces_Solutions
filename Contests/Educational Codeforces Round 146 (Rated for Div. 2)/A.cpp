f40eb246a9a12c301d51a8643b33026343e389c2
Anowar Hossen Farvez <farvez165@gmail.com>
2023-04-16 23:13:11 +0600

    Accepted

---
+++ Contests/Educational Codeforces Round 146 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,62 @@
+//  Bismillahir Rahmanir Raheem //
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <cmath>
+#define readFirst() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+#define ll long long int
+#define vll vector<long long int>
+#define mll map<long long int, long long int>
+#define pb push_back
+#define pp pop_back
+#define gcd(x, y) __gcd(x, y)
+#define pi M_PI
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+using namespace std;
+
+bool is_prime(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+ll digits_sum(ll n)
+{
+    ll sum = 0;
+    while (n)
+    {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+}
+
+void solve(){   
+    ll n,k;
+    cin>>n>>k;
+    if(n%2==k%2 || k==1 || n%2==0){
+        YES;
+    }
+    else{
+        NO;
+    }  
+}
+
+int main()
+{
+    readFirst();
+    ll tCases = 1;
+    cin >> tCases;
+    while (tCases--)
+    {
+        solve();
+    }
+    return 0;
+}
